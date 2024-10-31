839b03c6bc50ffabfb5f01cc0613561e17b2f6ec
daebon07 <muhammadshabran25@gmail.com>
2024-10-31 21:00:20 +0700

    jawaban pertanyaan 4 nomor 1

---
+++ searchnilai20.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class searchnilai20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Masukkan jumlah elemen array: ");
+        int jumlahElemen = sc.nextInt();
+
+        
+        int[] arrNilai = new int[jumlahElemen];
+
+        
+        System.out.println("Masukkan " + jumlahElemen + " nilai:");
+        for (int i = 0; i < jumlahElemen; i++) {
+            System.out.print("Nilai ke-" + (i + 1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
+
+        int hasil = -1; 
+
+        
+        for (int i = 0; i < arrNilai.length; i++) {
+            if (key == arrNilai[i]) {
+                hasil = i; 
+                break; 
+            }
+        }
+
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ditemukan di indeks ke-" + hasil);
+        } else {
+            System.out.println("Nilai " + key + " tidak ditemukan dalam array.");
+        }
+    }
+}
+
