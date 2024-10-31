75d6179c04f3bcab5b3e0e28f42963f7dac2ce26
daebon07 <muhammadshabran25@gmail.com>
2024-10-31 20:45:44 +0700

    modifikasi pertanyaan 3 nomor 2

--- arrayratanilai20.java
+++ arrayratanilai20.java
@@ -4,7 +4,10 @@ public class arrayratanilai20 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in); 
 
-        int[] nilaiMhs = new int[10];
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMahasiswa = sc.nextInt(); 
+        
+        int[] nilaiMhs = new int[jumlahMahasiswa]; 
         double total = 0;
         double rata2;
         int jumlahLulus = 0; 
@@ -15,7 +18,7 @@ public class arrayratanilai20 {
             nilaiMhs[i] = sc.nextInt();
         }
 
-        
+       
         for (int i = 0; i < nilaiMhs.length; i++) {
             total += nilaiMhs[i];
             if (nilaiMhs[i] > 70) {
@@ -26,8 +29,10 @@ public class arrayratanilai20 {
         
         rata2 = total / nilaiMhs.length;
         
-        
+       
         System.out.println("Rata-rata nilai = " + rata2);
         System.out.println("Jumlah mahasiswa yang lulus = " + jumlahLulus);
     }
 }
+
+
