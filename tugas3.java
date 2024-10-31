10b482f73ab5e17eee220475b68f744f95483f8c
daebon07 <muhammadshabran25@gmail.com>
2024-10-31 21:14:15 +0700

    tugas 3

---
+++ tugas3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class tugas3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        String[] menu = {"Nasi Goreng", "Mie Goreng", "Roti Bakar", "Kentang Goreng", 
+                         "Teh Tarik", "Cappucino", "Chocolate Ice"};
+
+        
+        System.out.print("Masukkan nama makanan yang ingin dicari: ");
+        String makananDicari = sc.nextLine();
+
+        
+        boolean ditemukan = false;
+        for (String makanan : menu) {
+            if (makanan.equalsIgnoreCase(makananDicari)) {
+                ditemukan = true;
+                break; 
+            }
+        }
+
+        
+        if (ditemukan) {
+            System.out.println("Makanan \"" + makananDicari + "\" tersedia di menu.");
+        } else {
+            System.out.println("Makanan yang dicari tidak ada di menu.");
+        }
+    }
+}
+
