ec5b626183c17c60278a948a87827594e1d19efb
daebon07 <muhammadshabran25@gmail.com>
2024-10-31 19:57:27 +0700

    modifikasi percobaan 2

--- arraynilai20.java
+++ arraynilai20.java
@@ -4,13 +4,15 @@ public class arraynilai20 {
         int [] nilaiakhir= new int [10];
         Scanner sc = new Scanner (System.in); 
 
-        for (int i = 0; i <10; i++){
+        for (int i = 0; i <nilaiakhir.length; i++){
             System.out.print("masukan nilai akhir ke " + i + " = ");
             nilaiakhir[i] = sc.nextInt ();
         }
         for (int i = 0; i <10; i++){
-            System.out.println("nilai akhir ke " + i + " adalah " + nilaiakhir[i]);
-            
-        }
+           if (nilaiakhir[i]>70 ) {
+            System.out.println("mahasiswa ke " + i + " lulus " );
+            }
+            else  System.out.println("mahasiswa ke " + i + " tidak lulus " );{}
+    }
     }
 }
